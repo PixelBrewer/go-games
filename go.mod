1a7d51ee97472e498a608d5518a0a90a6ff22850
pixelbrewer <pixelbrewer@protonmail.com>
2026-02-18 00:30:24 -0800

    Adding final commit for the night
    Adding basic app scaffold and testing out some functions. Also adding a
    sql folder to seed my db.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pixelbrewer/go-games
+
+go 1.25.0
---
+++ internal/game.go
@@ -0,0 +1,11 @@
+// This is a data model for a game
+package game
+
+type game struct {
+	title                string
+	completed            bool
+	description          string
+	completionPercentage int
+	platform             string
+	trophyHunting        bool
+}
